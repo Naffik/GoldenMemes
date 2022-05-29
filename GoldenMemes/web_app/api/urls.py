8f0639a70bdc7725d507f76b647694d161ea61e4
Jakub <jakub.ziemba32@gmial.com>
2022-05-29 15:21:27 +0200

    implementation of the JWT token authentication

--- GoldenMemes/web_app/api/urls.py
+++ GoldenMemes/web_app/api/urls.py
@@ -12,8 +12,8 @@ urlpatterns = [
     path(r'comment/', CommentList.as_view(), name='comment-list'),
     path(r'comment/<int:pk>/', CommentDetail.as_view(), name='comment-detail'),
 
-    path(r'post/<int:pk>/create_comment/', CommentCreate.as_view(), name='comment-create'),
+    path(r'post/<int:pk>/create-comment/', CommentCreate.as_view(), name='comment-create'),
     path(r'post/', PostList.as_view(), name='post-list'),
     path(r'post/<int:pk>/', PostDetail.as_view(), name='post-detail'),
-    path(r'post/create_post', PostCreate.as_view(), name='post-create'),
+    path(r'post/create-post', PostCreate.as_view(), name='post-create'),
 ]
