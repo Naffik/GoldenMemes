e68a928102e2049c0d655d901732848409c5ea05
Naffik <76112481+Naffik@users.noreply.github.com>
2023-04-03 15:31:25 +0200

    Delete GoldenMemes/frontend directory

--- GoldenMemes/frontend/src/components/ErrorMessage.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styles from "./ErrorMessage.module.scss";
-
-function ErrorMessage({ message, styling = null }) {
-  return <p className={`${styles.error} ${styling && styling.map((style) => styles[style])}`}>{message}</p>;
-}
-
-export default ErrorMessage;
